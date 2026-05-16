b92111c49daf5743a268859d4610d563d7a13865
Simenza <federico.giuliana12@gmail.com>
2026-05-16 14:09:20 +0200

    Changed name and fixed some issues

---
+++ launch/robot.launch.py
@@ -0,0 +1,252 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+
+
+def generate_launch_description():
+    # Define the robot's name and package name
+    robot_name = "cleanbit"
+    package_name = "cleanbit_simulate"
+    world_file_name = "laboratory.sdf"
+    
+
+    
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(
+            get_package_share_directory(package_name),
+            'worlds',
+            world_file_name
+        ),
+        description='Full path to the world model file to load'
+    )
+
+    # Define launch arguments for initial pose
+    x_arg = DeclareLaunchArgument(
+        'x', default_value='0.0', description='Initial X position')
+
+    y_arg = DeclareLaunchArgument(
+        'y', default_value='0.0', description='Initial Y position')
+
+    z_arg = DeclareLaunchArgument(
+        'z', default_value='0.5', description='Initial Z position')
+
+    roll_arg = DeclareLaunchArgument(
+        'R', default_value='0.0', description='Initial Roll')
+
+    pitch_arg = DeclareLaunchArgument(
+        'P', default_value='0.0', description='Initial Pitch')
+
+    yaw_arg = DeclareLaunchArgument(
+        'Y', default_value='0.0', description='Initial Yaw')
+
+    # Retrieve launch configurations
+    world_file = LaunchConfiguration('world')
+    x = LaunchConfiguration('x')
+    y = LaunchConfiguration('y')
+    z = LaunchConfiguration('z')
+    roll = LaunchConfiguration('R')
+    pitch = LaunchConfiguration('P')
+    yaw = LaunchConfiguration('Y')
+
+    # Set paths to Xacro model and configuration files
+    robot_model_path = os.path.join(
+        get_package_share_directory(package_name),
+        'model',
+        'robot.xacro'
+    )
+
+    gz_bridge_params_path = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'gz_bridge.yaml'
+    )
+
+    joy_params_path = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'joystick.yaml'
+    )
+
+    slam_params_path = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'mapper_params_online_async.yaml'
+    )
+
+    twist_mux_params_path = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'twist_mux.yaml'
+    )
+
+    nav2_params_path = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'nav2_params.yaml'
+    )
+
+    rviz_config = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'cleanbit_rviz.rviz'
+    )
+
+
+    # Process the Xacro file to generate the URDF representation of the robot
+    robot_description = xacro.process_file(robot_model_path).toxml()
+
+    # Prepare to include the Gazebo simulation launch file
+    gazebo_pkg_launch = PythonLaunchDescriptionSource(
+        os.path.join(
+            get_package_share_directory('ros_gz_sim'),
+            'launch',
+            'gz_sim.launch.py'
+        )
+    )
+
+    # Include the Gazebo launch description with specific arguments
+    gazebo_launch = IncludeLaunchDescription(
+        gazebo_pkg_launch,
+        launch_arguments={
+            'gz_args': [f'-r -v 4 ', world_file],
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
+
+    # Create a node to spawn the robot model in the Gazebo environment
+    spawn_model_gazebo_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', robot_name,
+            '-string', robot_description,
+            '-x', x,
+            '-y', y,
+            '-z', z,
+            '-R', roll,
+            '-P', pitch,
+            '-Y', yaw,
+            '-allow_renaming', 'false'
+        ],
+        output='screen',
+    )
+
+    # Create a node to publish the robot's state based on its URDF description
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[
+            {'robot_description': robot_description, 'use_sim_time': True}
+        ],
+        output='screen'
+    )
+
+    # Create a node for the ROS-Gazebo bridge to handle message passing
+    gz_bridge_node = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args', '-p',
+            f'config_file:={gz_bridge_params_path}'
+        ],
+        output='screen'
+    )
+
+    # Create a node for the Odom - TF transformation
+    odom_to_tf_node = Node(
+        package=package_name,
+        executable='odom_to_tf.py',
+        name='odom_to_tf',
+        output='screen'
+    )
+
+    joy_node = Node(
+            package='joy',
+            executable='joy_node',
+            parameters=[joy_params_path],
+        )
+    
+    teleop_node = Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            name='teleop_twist_joy_node',
+            parameters=[joy_params_path],
+            remappings=[('/cmd_vel', '/cmd_vel_joy')]
+        )
+
+
+    slam_toolbox_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('slam_toolbox'),
+                'launch',
+                'online_async_launch.py'
+            )
+        ),
+        launch_arguments={
+            'slam_params_file': slam_params_path,
+            'use_sim_time': 'true'
+        }.items()
+    )
+
+
+    twist_mux_process = Node(
+        package="twist_mux",
+        executable="twist_mux",
+        name="twist_mux",
+        output="screen",
+        parameters=[twist_mux_params_path],
+        remappings=[("cmd_vel_out", "cmd_vel")]
+    )
+
+    nav2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('nav2_bringup'),
+                'launch',
+                'navigation_launch.py'
+            )
+        ),
+        launch_arguments={
+            'params_file': nav2_params_path,
+            'use_sim_time': 'true'
+        }.items()
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        world_arg,
+        x_arg,
+        y_arg,
+        z_arg,
+        roll_arg,
+        pitch_arg,
+        yaw_arg,
+        gazebo_launch,
+        spawn_model_gazebo_node,
+        robot_state_publisher_node,
+        gz_bridge_node,
+        odom_to_tf_node,
+        joy_node,
+        teleop_node,
+        slam_toolbox_launch,
+        nav2_launch,
+        twist_mux_process,
+        rviz_node
+    ])
